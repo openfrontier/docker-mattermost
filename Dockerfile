be0535e82c3faae7017601ffb894c1f0bc03f1e1
张 新涛 <zhang.xintao@trans-cosmos.com.cn>
2018-10-26 15:02:09 +0800

    init push
    Change-Id: I12cdc0222c9df35b27e3c514a87aa2b700c6ff1c
    Signed-off-by: 张 新涛 <zhang.xintao@trans-cosmos.com.cn>

---
+++ Dockerfile
@@ -0,0 +1,50 @@
+FROM alpine:3.8
+
+# Some ENV variables
+ENV PATH="/mattermost/bin:${PATH}"
+
+# Build argument to set Mattermost edition
+ARG edition=team
+ARG MATTERMOST_VERSION=5.4.0
+ARG PUID=2000
+ARG PGID=2000
+
+# Install some needed packages
+RUN apk add --no-cache \
+	ca-certificates \
+	curl \
+	jq \
+	libc6-compat \
+	libffi-dev \
+	linux-headers \
+	mailcap \
+	netcat-openbsd \
+	xmlsec-dev \
+	&& rm -rf /tmp/*
+
+# Get Mattermost
+RUN mkdir -p /mattermost/data /mattermost/plugins /mattermost/client/plugins \
+    && if [ "$edition" = "team" ] ; then curl https://releases.mattermost.com/$MATTERMOST_VERSION/mattermost-team-$MATTERMOST_VERSION-linux-amd64.tar.gz | tar -xvz ; \
+      else curl https://releases.mattermost.com/$MATTERMOST_VERSION/mattermost-$MATTERMOST_VERSION-linux-amd64.tar.gz | tar -xvz ; fi \
+    && cp /mattermost/config/config.json /config.json.save \
+    && rm -rf /mattermost/config/config.json \
+    && addgroup -g ${PGID} mattermost \
+    && adduser -D -u ${PUID} -G mattermost -h /mattermost -D mattermost \
+    && chown -R mattermost:mattermost /mattermost /config.json.save /mattermost/plugins /mattermost/client/plugins
+
+USER mattermost
+
+#Healthcheck to make sure container is ready
+HEALTHCHECK CMD curl --fail http://localhost:8000 || exit 1
+
+# Configure entrypoint and command
+COPY entrypoint.sh /
+ENTRYPOINT ["/entrypoint.sh"]
+WORKDIR /mattermost
+CMD ["mattermost"]
+
+# Expose port 8000 of the container
+EXPOSE 8000
+
+# Declare volumes for mount point directories
+VOLUME ["/mattermost/data", "/mattermost/logs", "/mattermost/config", "/mattermost/plugins", "/mattermost/client/plugins"]
---
+++ entrypoint.sh
@@ -0,0 +1,100 @@
+#!/bin/sh
+
+# Read environment variables or set default values
+DB_HOST=${DB_HOST:-db}
+DB_PORT_NUMBER=${DB_PORT_NUMBER:-5432}
+MM_USERNAME=${MM_USERNAME:-mmuser}
+MM_PASSWORD=${MM_PASSWORD:-mmuser_password}
+MM_DBNAME=${MM_DBNAME:-mattermost}
+MM_CONFIG=${MM_CONFIG:-/mattermost/config/config.json}
+
+if [ "${1:0:1}" = '-' ]; then
+    set -- mattermost "$@"
+fi
+
+if [ "$1" = 'mattermost' ]; then
+  # Check CLI args for a -config option
+  for ARG in $@;
+  do
+      case "$ARG" in
+          -config=*)
+              MM_CONFIG=${ARG#*=};;
+      esac
+  done
+
+  if [ ! -f $MM_CONFIG ]
+  then
+    # If there is no configuration file, create it with some default values
+    echo "No configuration file" $MM_CONFIG
+    echo "Creating a new one"
+    # Copy default configuration file
+    cp /config.json.save $MM_CONFIG
+    # Substitue some parameters with jq
+    #GENERAL
+    jq '.ServiceSettings.SiteURL = "'${MATTERMOST_SITEURL}'"' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    jq '.ServiceSettings.ListenAddress = ":8000"' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    jq '.TeamSettings.EnableUserCreation = false' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    jq '.TeamSettings.EnableTeamCreation = false' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    jq '.TeamSettings.MaxUsersPerTeam = 20000' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    jq '.TeamSettings.RestrictDirectMessage = "team"' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    jq '.TeamSettings.TeammateNameDisplay = "full_name"' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    jq '.LogSettings.EnableConsole = false' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    jq '.LogSettings.EnableDiagnostics = false' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    jq '.LogSettings.ConsoleJson = false' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    #SECURITY
+    jq '.EmailSettings.RequireEmailVerification = true' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    jq '.FileSettings.EnablePublicLink = true' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    #NOTIFICATIONS
+    jq '.EmailSettings.SendEmailNotifications = true' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    jq '.EmailSettings.EnablePreviewModeBanner = false' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    jq '.EmailSettings.EnableEmailBatching = true' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    jq '.EmailSettings.FeedbackName = "Mattermost Notification"' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    jq '.EmailSettings.FeedbackEmail = "'${MATTERMOST_EMAIL_FEEDBACKEMAIL}'"' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    jq '.EmailSettings.FeedbackOrganization = "'${MATTERMOST_EMAIL_FEEDBACKORGANIZATION}'"' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    jq '.EmailSettings.SMTPServer = "'${MATTERMOST_EMAIL_SMTPSERVER}'"' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    jq '.EmailSettings.SMTPPort = "'${MATTERMOST_EMAIL_SMTPPORT}'"' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    jq '.EmailSettings.EnableSMTPAuth = '${MATTERMOST_EMAIL_ENABLESMTPAUTH}'' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    jq '.EmailSettings.SMTPUsername = "'${MATTERMOST_EMAIL_SMTPUSERNAME}'"' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    jq '.EmailSettings.SMTPPassword = "'${MATTERMOST_EMAIL_SMTPPASSWORD}'"' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    jq '.EmailSettings.ConnectionSecurity = "'${MATTERMOST_EMAIL_CONNECTIONSECURITY}'"' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    jq '.EmailSettings.SendPushNotifications = true' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    jq '.EmailSettings.PushNotificationServer = "'${MATTERMOST_EMAIL_PUSHNOTIFICATIONSERVER}'"' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    jq '.EmailSettings.PushNotificationContents = "full"' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG    
+    #INTEGRATIONS
+    jq '.ServiceSettings.EnableOAuthServiceProvider = true' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    jq '.ServiceSettings.EnablePostUsernameOverride = true' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    jq '.ServiceSettings.EnablePostIconOverride = true' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    jq '.ServiceSettings.EnableUserAccessTokens = true' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    #FILES
+    jq '.FileSettings.AmazonS3SSL = false' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    #CUSTOMIZATION
+    jq '.ServiceSettings.EnableCustomEmoji = true' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    jq '.ServiceSettings.EnableLinkPreviews = true' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    #ADVANCED
+    jq '.RateLimitSettings.VaryByUser = true' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    jq '.RateLimitSettings.Enable = true' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    jq '.SqlSettings.DriverName = "'${MATTERMOST_SQL_DRIVERNAME}'"' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    jq '.SqlSettings.MaxOpenConns = '${MATTERMOST_SQL_MAXOPENCONNS}'' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
+    # Configure database access
+    if [[ -z "$MM_SQLSETTINGS_DATASOURCE" ]]
+    then
+      echo -ne "Configure database connection..."
+      # URLEncode the password, allowing for special characters
+      ENCODED_PASSWORD=$(printf %s $MM_PASSWORD | jq -s -R -r @uri)
+      export MM_SQLSETTINGS_DATASOURCE="postgres://$MM_USERNAME:$ENCODED_PASSWORD@$DB_HOST:$DB_PORT_NUMBER/$MM_DBNAME?sslmode=disable&connect_timeout=10"
+      echo OK
+    else
+      echo "Using existing database connection"
+    fi
+  else
+    echo "Using existing config file" $MM_CONFIG
+  fi
+
+  # Wait another second for the database to be properly started.
+  # Necessary to avoid "panic: Failed to open sql connection pq: the database system is starting up"
+  sleep 1
+
+  echo "Starting mattermost"
+fi
+
+exec "$@"
